9b863213e63ebe39eff6b13c3cb3b14411369b12
João Lucca Trench <jltrench@gmail.com>
2024-01-20 22:48:43 -0300

    basic landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">Journal App</h1>
+        <p className="text-2xl text-white/55 mb-4">Track your mood</p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-blue-500 px-4 py-2 rounded-lg text-xl">
+              get started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
